1a143f6f3081a51ae6cc245e4fe21ff61d1e5b6a
BreacherBros <breacherbros@gmail.com>
2025-10-26 14:04:18 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.158.0/build/three.m
 import { PointerLockControls } from 'https://cdn.jsdelivr.net/npm/three@0.158.0/examples/jsm/controls/PointerLockControls.js';
 
 
+
 import { Weapon } from './weapons.js';
 import { Enemy } from './enemy.js';
 import { EnemyAI } from './enemyAI.js';
