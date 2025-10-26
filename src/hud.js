c511d39a2268977603d8db5db160b73a1a8d0667
BreacherBros <breacherbros@gmail.com>
2025-10-26 12:59:28 +0100

    Update hud.js

--- src/hud.js
+++ src/hud.js
@@ -1,3 +1,5 @@
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.158.0/build/three.module.js';
+
 export function updateHUD(player, hud, level) {
     hud.health.innerText = `Leben: ${Math.max(player.health,0)}`;
     hud.ability.innerText = `Fähigkeit: ${player.ability}`;
