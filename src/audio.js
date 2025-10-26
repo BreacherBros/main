ee6bcdeb1afa360d5bc7f9cc4685483040fef218
BreacherBros <breacherbros@gmail.com>
2025-10-26 12:58:48 +0100

    Update audio.js

--- src/audio.js
+++ src/audio.js
@@ -1,3 +1,5 @@
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.158.0/build/three.module.js';
+
 export class AudioManager {
     constructor(listener){
         this.listener = listener;
