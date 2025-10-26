484302845f9ea35d2ff94ba8780ac1e45e04d135
BreacherBros <breacherbros@gmail.com>
2025-10-26 14:33:37 +0100

    Update hud.js

--- src/hud.js
+++ src/hud.js
@@ -1,8 +1,19 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.158.0/build/three.module.js';
 
 
-export function updateHUD(player, hud, level) {
-    hud.health.innerText = `Leben: ${Math.max(player.health,0)}`;
-    hud.ability.innerText = `Fähigkeit: ${player.ability}`;
-    hud.level.innerText = `Level: ${level}`;
+export class HUD {
+    constructor(healthEl, ammoEl, abilityEl, levelEl) {
+        this.healthEl = healthEl;
+        this.ammoEl = ammoEl;
+        this.abilityEl = abilityEl;
+        this.levelEl = levelEl;
+    }
+
+    update(player, levelManager) {
+        this.healthEl.innerText = `Leben: ${Math.max(player.health, 0)}`;
+        this.ammoEl.innerText = player.weapon?.reloading ? "Nachladen..." : `Munition: ${player.weapon?.ammo}/${player.weapon?.maxAmmo}`;
+        this.abilityEl.innerText = `Fähigkeit: ${player.ability} (${Math.max(player.abilityObj?.cooldown.toFixed(1), 0)}s)`;
+        this.levelEl.innerText = `Level: ${levelManager?.level}`;
+    }
 }
+
