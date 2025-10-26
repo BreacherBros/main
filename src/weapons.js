d5d5bee0890431e6dc806ee1490e6e8ea23b4992
BreacherBros <breacherbros@gmail.com>
2025-10-26 13:03:03 +0100

    Update weapons.js

--- src/weapons.js
+++ src/weapons.js
@@ -1,5 +1,6 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.158.0/build/three.module.js';
 
+
 export class Weapon {
     constructor(player, scene, hud) {
         this.player = player;
